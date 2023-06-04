7984ed9f4a7cf1b0f3699af11d05354db0325a53
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-06-03 23:26:32 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+<html lang="pt-BR">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="Refresh" content="120" />
+    <meta name="viewport" content="width=device-width, initial-scale=0.7" />
+    <link rel="stylesheet" href="/src/css/style.css" />
+    <link rel="stylesheet" href="https://pingobras.glitch.me/css/btn.css" />
+    <link rel="stylesheet" href="https://pingobras.glitch.me/css/head.css" />
+
+    <title> DATA SYSTEM </title>
+  </head>
+  <body>
+ 
+    
+    
+    <footer>
+      <div class="links"></div>
+
+      <div id="direitos" class="direitos">Loading...</div>
+    </footer>
+  </body>
+  <script src="/src/js/dataSys.js" defer></script>
+  <script src="https://pingobras.glitch.me/js/copyright.js" defer></script>
+  <host>
+    <script src="https://pingobras.glitch.me/js/load-host.js" defer></script>
+    </host>
+</html>
