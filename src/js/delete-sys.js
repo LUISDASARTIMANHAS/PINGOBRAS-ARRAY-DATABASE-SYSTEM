33cdeaf0962d17f539c9e99d4ae99389ed188849
LUIS_DAS_ARTIMANHAS <75493473+LUISDASARTIMANHAS@users.noreply.github.com>
2023-06-09 15:44:35 -0300

    Create delete-sys.js

---
+++ src/js/delete-sys.js
@@ -0,0 +1,38 @@
+let data = []
+const DB = localStorage.getItem("data");
+
+if(!DB){
+  console.log("-------DATA-SYSTEM------")
+  console.log("erro: Não implementado ou salvo!")
+  console.log("erro: " + DB)
+}else{
+   data = JSON.parse(DB)
+  
+}
+
+function removeData(search){
+let select = JSON.stringify(data[search])
+let posicao = data.indexOf(data[search]);
+if (posicao > -1) {
+  data.splice(posicao, 1);
+  console.log("Item deletado: "+select)
+}else{
+  console.log("<err> Impossível deletar: Item Não Encontrado no banco de dados")
+}
+readData()
+}
+
+function readData(){
+  let dataString = JSON.stringify(data)
+  let bytes = dataString.length
+  let kBytes = (bytes/1000).toFixed(2)
+  let mBytes = (kBytes/1000).toFixed(2)
+  
+  console.log("-------DATA-SYSTEM------");
+  console.log("DATA: " + dataString);
+  console.log("TAMANHO: " + data.length);
+  console.log("TAMANHO(Bytes): " + bytes);
+  console.log("TAMANHO(KB): " + kBytes);
+  console.log("TAMANHO(MB): " + mBytes);
+}
+readData()
