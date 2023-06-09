db2051fb73eab71eded7f320469904827e62447c
Glitch (pingobras-array-database-system) <none>
2023-06-09 23:11:45 +0000

    🍇🥥 Updated with Glitch

---
+++ src/js/post-sys.js
@@ -0,0 +1,61 @@
+const DB = localStorage.getItem("data");
+const DBLogs = localStorage.getItem("logs");
+const user = document.getElementById("user");
+const senha = document.getElementById("senha");
+const bio = document.getElementById("bio");
+let data = ["No Data"]
+let dataLogs = ["Inicialização"];
+
+if(!DB || DB == "[]" || !DBLogs){
+  console.log("-------DATA-SYSTEM------")
+  console.log("Banco de dados Não implementado ou salvo!")
+  console.log("erro: " + DB)
+}else{
+   data = JSON.parse(DB)
+  console.log("Banco de dados carregado!")
+  
+}
+
+
+function gerarDados(){
+  let userData  = {
+"usuario":user.value,
+"senha":senha.value,
+"bio":bio.value
+}
+  logs("Novo Dado Gerado")
+  postData(userData)
+}
+
+function postData(item){
+data.push(item)
+readData()
+  
+let dataString = JSON.stringify(data)
+localStorage.setItem("data",dataString);
+logs("O dado foi salvo!")
+}
+
+function readData(){
+  let dataString = JSON.stringify(data)
+  let bytes = dataString.length
+  let kBytes = (bytes/1000).toFixed(2)
+  let mBytes = (kBytes/1000).toFixed(2)
+  logs("Leitura de dados...")
+  
+  console.log("-------DATA-SYSTEM------");
+  console.log("DATA: " + dataString);
+  console.log("TAMANHO: " + data.length);
+  console.log("TAMANHO(Bytes): " + bytes);
+  console.log("TAMANHO(KB): " + kBytes);
+  console.log("TAMANHO(MB): " + mBytes);
+  
+}
+readData()
+
+function logs(log){
+  dataLogs.push(log);
+
+  let dataLogsStr = JSON.stringify(dataLogs);
+  localStorage.setItem("logs", dataLogsStr);
+}
